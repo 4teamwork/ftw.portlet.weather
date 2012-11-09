9071549fb5a4e6e17fc5a0d9710d579705b8d870
Jonas Baumann <jone@jone.ch>
2012-11-09 08:51:50 +0100

    Remove test_registered_interfaces: its testing plone, which changes from 4.1 to 4.2.

--- ftw/portlet/weather/tests/test_weather_portlet.py
+++ ftw/portlet/weather/tests/test_weather_portlet.py
@@ -2,7 +2,6 @@ from plone.portlets.interfaces import IPortletType
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletRenderer
 from zope.component import getUtility, getMultiAdapter
-from Products.GenericSetup.utils import _getDottedName
 from ftw.portlet.weather import portlet as weather
 from ftw.portlet.weather.testing import (
     FTW_WEATHERPORTLET_INTEGRATION_TESTING)
@@ -21,14 +20,6 @@ class TestPortlet(unittest.TestCase):
             IPortletType, name='ftw.portlet.weather')
         self.assertEquals(
             portlet.addview, 'ftw.portlet.weather')
-
-    def test_registered_interfaces(self):
-        portlet = getUtility(
-            IPortletType, name='ftw.portlet.weather')
-        registered_interfaces = [_getDottedName(i) for i in portlet.for_]
-        registered_interfaces.sort()
-        self.assertEquals(
-            ['zope.interface.Interface', ], registered_interfaces)
 
     def test_interfaces(self):
         portlet = weather.Assignment()
