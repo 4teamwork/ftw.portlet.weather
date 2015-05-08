165a89812271829d9c6a27a6612579289cddf707
Jonas Baumann <jone@jone.ch>
2015-05-08 12:09:58 +0200

    4teamwork is an AG.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='ftw.portlet.weather',
         ],
 
       keywords='ftw portlet weather',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.portlet.weather',
       license='GPL2',
